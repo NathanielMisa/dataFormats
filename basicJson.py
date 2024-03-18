84a12bee588a7fdd421384944181ba259887ddec
nm <jamesmisa18@gmail.com>
2024-03-18 17:08:47 +1300

    added more functions

--- basicJson.py
+++ basicJson.py
@@ -26,12 +26,23 @@ def getSwitch(dataDict, location = ""):
     if missingLocation == True:
         print(f"No switches found in location: {location}")
   
-
-
+def addNetworkDevice(detailObject, deviceType, data):
+    data["networkDevices"][deviceType].append(detailObject)
+    newData = json.dumps(data, indent = 4)
+    print(newData) #I don't actually want to create a new json file, for prac purposes
+    
 
 def main():  #this is where the user can change the script to get their desired results
     myData = readJson()
-    getSwitch(myData, "")
+    
+    getSwitch(myData, "auckland")
+    
+    newRouter = {
+        "hostname": "router1-ham",
+        "ip address": "172.16.4.1",
+        "location": "Hamilton"
+    }
+    addNetworkDevice(newRouter,"routers", myData)
 
 
 if __name__ == "__main__":
